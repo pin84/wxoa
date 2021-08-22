9e66e01e3699377c7ee05cdee07b6e7756c51860
pin84 <qp1984@live.com>
2021-08-22 10:06:34 +0800

    save

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,13 @@
-// const baseURL = 'http://localhost:3000'
-// const baseURL = 'http://data.lzhs.top'
-const baseURL = 'http://42ml122265.wicp.vip'
+// const baseURL = 'http://42ml122265.wicp.vip'
+let baseURL
+let env = process.env.NODE_ENV
+if (env == 'development') {
+    baseURL = 'http://localhost:3000'
+} else {
+    baseURL = 'http://data.lzhs.top'
+}
 
+console.log('----baseURL---',baseURL);
 
 
 export {
