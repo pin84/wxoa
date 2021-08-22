feb9b5cd1b5cd0ddec4260563348f80be367bfe4
pin84 <qp1984@live.com>
2021-08-22 09:25:54 +0800

    save

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // const baseURL = 'http://localhost:3000'
-const baseURL = 'http://data.lzhs.top'
-// const baseURL = 'http://42ml122265.wicp.vip'
+// const baseURL = 'http://data.lzhs.top'
+const baseURL = 'http://42ml122265.wicp.vip'
 
 
 
