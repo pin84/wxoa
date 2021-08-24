ce89ca45c7f6fb1e128c972d05fa625f05a5602c
pin84 <qp1984@live.com>
2021-08-24 14:48:04 +0800

    save

--- src/views/qrcode/ImageQr.vue
+++ src/views/qrcode/ImageQr.vue
@@ -53,6 +53,8 @@ export default {
         sourceType: ["album", "camera"], // 可以指定来源是相册还是相机，默认二者都有
         success: function (res) {
           var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+
+          console.log('---localIds-',localIds);
           wx.getLocalImgData({
             localId: localIds[0], // 图片的localID
             success: function (res) {
