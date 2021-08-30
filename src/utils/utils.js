395ef57aed6b54d5e532d0f9353d7974dae5ffdd
pin84 <qp1984@live.com>
2021-08-30 11:56:27 +0800

    save

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,32 @@
-const base64ToBlob = async (imgURL) => {
-  const base64 = await fetch(imgURL);
-  const blob = await base64.blob();
-  console.log('---blog---',blob);
-  return blob
-} 
+const base64ToBlob = async (dataURI) => {
+  var byteString = atob(dataURI.split(',')[1]);
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
 
+  let blob = new Blob([ab], { type: mimeString });
+  console.log('-blob--', blob);
+  return blob
+}
+
+
+
+
+//blob => base64
+// convertBlobToBase64 = (blob) => new Promise((resolve, reject) => {
+//   const reader = new FileReader;
+//   reader.onerror = reject;
+//   reader.onload = () => {
+//       resolve(reader.result);
+//   };
+//   reader.readAsDataURL(blob);
+// });
+
+// const base64String = await convertBlobToBase64(blob);
+// https://ionicframework.com/blog/converting-a-base64-string-to-a-blob-in-javascript/
 
 
 // 将blob转换为file
